8dcefb3cd2b9d8d2f198c3ecc83456bb3074c049
Leatpt <lea.kibur@tptlive.ee>
2025-05-13 18:18:46 +0300

    Adding DbSet Context to My Classes

--- KooliProjekt/Data/ApplicationDbContext.cs
+++ KooliProjekt/Data/ApplicationDbContext.cs
@@ -9,5 +9,13 @@ namespace KooliProjekt.Data
             : base(options)
         {
         }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<InvoiceLine> InvoiceLines { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Registration> Registrations { get; set; }
+
     }
 }
