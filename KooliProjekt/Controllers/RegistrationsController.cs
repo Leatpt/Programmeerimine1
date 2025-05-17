c9b7ffe8b40aeab2a1d9306a68e7daa048839b37
Leatpt <lea.kibur@tptlive.ee>
2025-05-17 13:51:33 +0300

    Adding Controllers for my Classes, changing the Header

---
+++ KooliProjekt/Controllers/RegistrationsController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using KooliProjekt.Data;
+
+namespace KooliProjekt.Controllers
+{
+    public class RegistrationsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RegistrationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Registrations
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Registrations.Include(r => r.Customer).Include(r => r.Event);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Registrations/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var registration = await _context.Registrations
+                .Include(r => r.Customer)
+                .Include(r => r.Event)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (registration == null)
+            {
+                return NotFound();
+            }
+
+            return View(registration);
+        }
+
+        // GET: Registrations/Create
+        public IActionResult Create()
+        {
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Address");
+            ViewData["EventId"] = new SelectList(_context.Events, "Id", "Description");
+            return View();
+        }
+
+        // POST: Registrations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,IsPaidEvent,CustomerId,EventId")] Registration registration)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(registration);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Address", registration.CustomerId);
+            ViewData["EventId"] = new SelectList(_context.Events, "Id", "Description", registration.EventId);
+            return View(registration);
+        }
+
+        // GET: Registrations/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var registration = await _context.Registrations.FindAsync(id);
+            if (registration == null)
+            {
+                return NotFound();
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Address", registration.CustomerId);
+            ViewData["EventId"] = new SelectList(_context.Events, "Id", "Description", registration.EventId);
+            return View(registration);
+        }
+
+        // POST: Registrations/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,IsPaidEvent,CustomerId,EventId")] Registration registration)
+        {
+            if (id != registration.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(registration);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RegistrationExists(registration.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Address", registration.CustomerId);
+            ViewData["EventId"] = new SelectList(_context.Events, "Id", "Description", registration.EventId);
+            return View(registration);
+        }
+
+        // GET: Registrations/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var registration = await _context.Registrations
+                .Include(r => r.Customer)
+                .Include(r => r.Event)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (registration == null)
+            {
+                return NotFound();
+            }
+
+            return View(registration);
+        }
+
+        // POST: Registrations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var registration = await _context.Registrations.FindAsync(id);
+            if (registration != null)
+            {
+                _context.Registrations.Remove(registration);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RegistrationExists(int id)
+        {
+            return _context.Registrations.Any(e => e.Id == id);
+        }
+    }
+}
